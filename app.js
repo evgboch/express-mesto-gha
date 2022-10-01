c0a5255ae274bc54d8d44e36b1a03c0a494773f7
Evgeny Bocharov <evgbocharov@gmail.com>
2022-10-02 00:58:44 +0300

    fix: исправить подключение к бд

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+// const bodyParser = require('body-parser');
 
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/mestodb', {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true,
-});
+// app.use(bodyParser.json());
+
+mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.listen(3000);
