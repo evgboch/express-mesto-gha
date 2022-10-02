8719700839dd60989fae71e4f95bea01e1f74702
Evgeny Bocharov <evgbocharov@gmail.com>
2022-10-02 19:47:46 +0300

    fix: исправить статус ответа при отправке некорректного пути

--- controllers/incorrectPath.js
+++ controllers/incorrectPath.js
@@ -1,4 +1,7 @@
+const { errorStatusList } = require('../utils/constants');
+
 function handleIncorrectPath(req, res) {
+  res.status(errorStatusList.notFound);
   res.send({
     message: 'Ошибка: указанный путь не существует',
   });
