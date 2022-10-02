ebfc1a287367eb14ba3700f60df63b1e9fa2118a
Evgeny Bocharov <evgbocharov@gmail.com>
2022-10-02 04:16:39 +0300

    feat: добавить временный мидлвэр для авторизации пользователя

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ const usersRouter = require('./routes/users');
 const app = express();
 
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+  req.user = {
+    _id: '6338e2e60ed9a72a2dcc38e0',
+  };
+
+  next();
+});
 app.use('/users', usersRouter);
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
