ba6d5dd6e168ba7b5457604398b57d617c7bafe2
Evgeny Bocharov <evgbocharov@gmail.com>
2022-10-11 18:24:52 +0300

    refactor: вынести роуты из файла app в отдельный файл

--- app.js
+++ app.js
@@ -2,14 +2,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
 const bodyParser = require('body-parser');
-const { errors, celebrate, Joi } = require('celebrate');
-const usersRouter = require('./routes/users');
-const cardsRouter = require('./routes/cards');
-const incorrectPathRouter = require('./routes/incorrectPath');
-const checkAuthorization = require('./middlewares/auth');
-const { createUser, login } = require('./controllers/users');
+const { errors } = require('celebrate');
 const catchErrors = require('./middlewares/errors');
-const { linkRegex } = require('./utils/constants');
+const mainRouter = require('./routes/index');
 
 const { PORT = 3000 } = process.env;
 
@@ -17,25 +12,7 @@ const app = express();
 
 app.use(helmet());
 app.use(bodyParser.json());
-app.post('/signin', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().required().email(),
-    password: Joi.string().required(),
-  }),
-}), login);
-app.post('/signup', celebrate({
-  body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
-    avatar: Joi.string().pattern(linkRegex),
-    email: Joi.string().required().email(),
-    password: Joi.string().required(),
-  }),
-}), createUser);
-app.use(checkAuthorization);
-app.use('/users', usersRouter);
-app.use('/cards', cardsRouter);
-app.use('*', incorrectPathRouter);
+app.use(mainRouter);
 app.use(errors());
 app.use(catchErrors);
 
