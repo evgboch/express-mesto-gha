fa88274e68e31ddc5a5a5e4fa7d12de47394d795
Evgeny Bocharov <evgbocharov@gmail.com>
2022-10-13 22:51:57 +0300

    feat: добавить логгер запросов и логгер ошибок

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ const mongoose = require('mongoose');
 const helmet = require('helmet');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
-const catchErrors = require('./middlewares/errors');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 const mainRouter = require('./routes/index');
+const catchErrors = require('./middlewares/errors');
 
 const { PORT = 3000 } = process.env;
 
@@ -12,7 +13,9 @@ const app = express();
 
 app.use(helmet());
 app.use(bodyParser.json());
+app.use(requestLogger);
 app.use(mainRouter);
+app.use(errorLogger);
 app.use(errors());
 app.use(catchErrors);
 
